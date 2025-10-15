91e39f6db809f6b2b030ac18f34477514a6c85c2
kasckepperd <uc2023216522@student.uc.pt>
2025-10-15 19:17:49 +0100

    atualizei downloader

--- downloader.java
+++ downloader.java
@@ -3,15 +3,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveAction;
+import java.util.HashMap;
 import java.io.IOException;
 
 
 
 public class downloader{
 
-public String url;
-public int paralel_threshold = 10;
+static String url;
+static ConcurrentLinkedQueue<String> urls = new ConcurrentLinkedQueue<String>();
+static HashMap<String,List<String>> index = new HashMap<>();
+static int parallel_threshold = 10;
 
 public Document download_page() throws IOException{
     return Jsoup.connect(url).get();
@@ -21,14 +26,47 @@ public String getText(Document doc){
     return doc.text();
 }
 
-public List<String> getUrls(Document doc){
-    List<String> urls = new ArrayList<>();
+public void addUrls(Document doc){
     Elements links = doc.select("a[href]");
     for(Element link: links){
         String url = link.absUrl("href");
         urls.add(url);
+    }     
+}
+
+public void putHashMap(String text,String url){
+    String[] words = text.split("\\W+");
+    for (String w :words){
+        if(!index.get(w).contains(url)){
+            index.keySet().add(url);
+        }
     }
-    return urls;      
+}
+
+public static void parallelIndexing(){
+    try(ForkJoinPool pool = new ForkJoinPool(parallel_threshold)){
+        pool.invoke(new Indexing(urls));
+    }
+}
+
+public static class Indexing extends RecursiveAction{
+    ConcurrentLinkedQueue<String> urls;
+    public Indexing(ConcurrentLinkedQueue<String> urls){
+        this.urls = urls;
+    }
+    public void compute(){
+        while(!urls.isEmpty()){
+        String url = urls.poll();
+        downloader robot = new downloader();
+        try{
+        Document doc = robot.download_page();
+        String texto = robot.getText(doc);
+        robot.addUrls(doc);
+        robot.putHashMap(texto,url);
+        }
+        catch(IOException e){}
+        }
+    }
 }
 
 public static void main(String[] args){
